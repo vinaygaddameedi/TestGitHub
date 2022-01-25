845c25afad21e5a3c7eaf5fce809f29d95f024ac
DELL <DELL@DESKTOP-610CKOB>
2022-01-25 16:04:20 +0530

    Handson1

---
+++ Handson3/src/com/cognizant/shapes/AreaCalculator.java
@@ -0,0 +1,11 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+	
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle();
+		r.calculateArea(10,20);
+		
+	}
+
+}
---
+++ Handson3/src/com/cognizant/shapes/Rectangle.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+public class Rectangle {
+	void calculateArea(int length,int breadth)
+	{
+		System.out.println("the area of the rectangle is :"+length*breadth);
+	}
+
+}
